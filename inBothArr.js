e02ea1e50a92d8faf4e73091a245649736149e70
Ruthie <65735332+lilyruth@users.noreply.github.com>
2021-02-09 18:04:25 -0600

    Add files via upload

---
+++ inBothArr.js
@@ -0,0 +1,17 @@
+// Write your code here:
+
+function justCoolStuff(firstArr, secondArr) {
+  let mutualArr = firstArr.filter( word => secondArr.includes(word));
+  return mutualArr;
+}
+
+
+// Feel free to uncomment the code below to test your function
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+
