be161f0c2c9035b75f0634d5fbfbb182b324b370
Ruthie <65735332+lilyruth@users.noreply.github.com>
2021-02-09 19:51:15 -0600

    Add files via upload

---
+++ teeth.js
@@ -0,0 +1,20 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+// Write your code here:
+
+
+function sortSpeciesByTeeth(anArr) {
+  return anArr.sort((specTeeth1, specTeeth2) => specTeeth1.numTeeth > specTeeth2.numTeeth);
+}
+
+
+// Feel free to comment out the code below when you're ready to test your function!
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
+
